0400783ef8b3d84e1a45edeacd01e6c6c94fe37f
Aniket Zimane <127310975+AniketZimane@users.noreply.github.com>
2024-04-21 12:13:56 +0530

    Update MySecurityConfig.java
    security commit

--- src/main/java/com/example/Organic_Food/Security/MySecurityConfig.java
+++ src/main/java/com/example/Organic_Food/Security/MySecurityConfig.java
@@ -34,9 +34,9 @@ public class MySecurityConfig
         http
                 .csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/user_assest/**", "/","/login/","/admin_assest/**","/admin_assest_bill/**","/master/login/","/data/pass/controller/","/new/user/registration/","/user/add/order/details/","/oder/placed/","/admin/payment_order/","/add/product/").permitAll()
+                .requestMatchers("/user_assest/**", "/","/login/","/admin_assest/**","/admin_assest_bill/**","/master/login/","/data/pass/controller/","/user/order/","/new/user/registration/","/user/add/order/details/","/oder/placed/","/admin/payment_order/","/add/product/").permitAll()
                 .requestMatchers("/product/","/feature/","/blog/","/add/feature/","/add/product/","/dashboard/","/scheme/","/add/scheme/","/admin/review/status/update/*","/review/report/","/admin/review/update/*","/admin/review/delete/*").hasRole("ADMIN")
-                .requestMatchers("/add/product/**","/","/addtocart/","/add/to/card/**","/user/order/","/add/to/card/*","/user/order/","/admin/payment_order/","/review/","/add/review/","/payment/").hasRole("USER")
+                .requestMatchers("/add/product/**","/","/addtocart/","/add/to/card/**","/add/to/card/*","/admin/payment_order/","/review/","/add/review/","/payment/").hasRole("USER")
                 .requestMatchers("/dashboard/").hasRole("FARMER")
                 .anyRequest()
                 .authenticated()
